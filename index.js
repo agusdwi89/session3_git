82e4776ecf506e3d60bc459b573d92094a0052c8
agus dwi <agusdwi89@gmail.com>
2020-12-02 13:45:42 +0800

    index.js selamat pagi

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 3000;
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send("Hello World");
+    res.send("Selamat pagi");
 })
 
 app.listen(port, () => console.log(`Hello listening on ${port} `));
